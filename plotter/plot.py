25176ce99ebd275a21df2c89348bc7b8aa9670c0
Martín Volpe <martin.volpe@gmail.com>
2014-11-25 00:37:08 -0300

    Added a simple script to plot x-y data.

---
+++ plotter/plot.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+FILE_LOCATION = 'test_data.txt' # default Location of data to be plotted
+
+def fetch_data(location):
+    # Open the file
+    x = np.array([])
+    y = np.array([])
+
+    with open(location) as datafile: # Wont handle the posible exceptions
+        for line in datafile:
+            tmp_x, tmp_y = line.split(" ") # example separator
+            x = np.append(x, float(tmp_x))
+            y = np.append(y, float(tmp_y))
+
+    return x, y
+
+def plot(location):
+    # Load data
+    x,y = fetch_data(location)
+
+    # Plot Data
+
+    plt.ylabel('Voltage [V]')
+    plt.ylabel('Time [s]')
+    plt.plot(x, y)
+
+    # Show or save plot
+    plt.show()
+    # plt.save()
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 2:
+        FILE_LOCATION = sys.argv[1]
+    plot(FILE_LOCATION)
